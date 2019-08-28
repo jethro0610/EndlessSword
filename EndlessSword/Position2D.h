9e832cecc2817087275ddb4116189db61ea1951d
JetDesktop <jethro0610@outlook.com>
2019-08-27 20:57:56 -0700

    Add equals operator overrides to Position2D

--- EndlessSword/Position2D.h
+++ EndlessSword/Position2D.h
@@ -18,20 +18,36 @@ struct Position2D {
 	Position2D operator+(const Position2D& OtherPosition) {
 		return Position2D(x + OtherPosition.x, y + OtherPosition.y);
 	}
+	Position2D operator+=(const Position2D& OtherPosition) {
+		x += OtherPosition.x;
+		y += OtherPosition.y;
+	}
 
 	// Subtract two positions
 	Position2D operator-(const Position2D& OtherPosition) {
 		return Position2D(x - OtherPosition.x, y - OtherPosition.y);
+	}
+	Position2D operator-=(const Position2D& OtherPosition) {
+		x -= OtherPosition.x;
+		y -= OtherPosition.y;
 	}
 
 	// Multiply a position by a float
 	Position2D operator*(const float& MultiplyFloat) {
 		return Position2D(x * MultiplyFloat, y * MultiplyFloat);
 	}
+	Position2D operator*=(const Position2D& OtherPosition) {
+		x *= OtherPosition.x;
+		y *= OtherPosition.y;
+	}
 
 	// Divide a position by a float
 	Position2D operator/(const float& DivideFloat) {
 		return Position2D(x / DivideFloat, y / DivideFloat);
+	}
+	Position2D operator/=(const Position2D& OtherPosition) {
+		x /= OtherPosition.x;
+		y /= OtherPosition.y;
 	}
 };
 
