1c33d1aeac4808fb568cee1e188cd9b3a854f8d6
JetDesktop <jethro0610@outlook.com>
2019-08-29 01:37:41 -0700

    Update main.cpp

--- EndlessSword/main.cpp
+++ EndlessSword/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <SDL.h>
 #include "GameWorld.h"
+#include "Geo.h"
+#include "GeoHole.h"
+#include "Diamond.h"
+#include "Hexagon.h"
+#include "Octagon.h"
+#include "Spawner.h"
+#include "TransformObject.h"
 
 int main(int argc, char** argv) {
 	// Start SDL
@@ -11,7 +18,13 @@ int main(int argc, char** argv) {
 		std::cout << "SDL initialized" << std::endl;
 	}
 
+	IMG_Init(IMG_INIT_PNG);
+	
 	GameWorld gameWorld = GameWorld(800, 800);
+	Spawner* spawner = new Spawner(100);
+
+	GeoHole* hole = new GeoHole();
+	hole->position = Vector2D(400, 400);
 
 	// Run the game
 	while (!gameWorld.WindowIsClosed()) {
