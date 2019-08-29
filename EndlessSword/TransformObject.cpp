b341113c9c8a9f822e1b5d780f96e4525b10bb1e
JetDesktop <jethro0610@outlook.com>
2019-08-28 22:11:20 -0700

    Add GetAllOverlaps to TransformObject

--- EndlessSword/TransformObject.cpp
+++ EndlessSword/TransformObject.cpp
@@ -9,6 +9,10 @@ TransformObject::~TransformObject() {
 }
 
 bool TransformObject::IsOverlapping(TransformObject* OtherObject) {
+	// Don't return true if the object is this one
+	if (OtherObject == this)
+		return false;
+
 	// Check if objects are overlapping one another using a circle
 	if (position.GetDistance(OtherObject->position) <= (scale / 2) + (OtherObject->scale/2)) {
 		return true;
@@ -16,6 +20,32 @@ bool TransformObject::IsOverlapping(TransformObject* OtherObject) {
 	else {
 		return false;
 	}
+}
+
+std::vector<TransformObject*> TransformObject::GetAllOverlaps() {
+	// Check overlap on all transform objects in the world
+	GameObject* currentObject = GameWorld::GetWorld()->GetFirstObject();
+	std::vector<TransformObject*> returnList;
+	while (currentObject != nullptr) {
+		// Cast to TransformObject
+		TransformObject* transformObject = dynamic_cast<TransformObject*>(currentObject);
+
+		// Cast is always null if the object is not of the desired type
+		if (transformObject != nullptr) {
+			if (IsOverlapping(transformObject)) {
+				returnList.push_back(transformObject); // Add to list if the object is overlapping
+			}
+		}
+
+		if (currentObject != GameWorld::GetWorld()->GetLastObject() && currentObject->GetNext() != nullptr) {
+			currentObject = currentObject->GetNext();
+		}
+		else {
+			break; // Stop loop if at the last object
+		}
+	}
+
+	return returnList;
 }
 
 Vector2D TransformObject::GetDrawPosition() {
--- EndlessSword/TransformObject.h
+++ EndlessSword/TransformObject.h
@@ -2,6 +2,7 @@
 #include "GameObject.h"
 #include "Vector2D.h"
 #include <SDL_stdinc.h>
+#include <vector>
 
 class TransformObject: public GameObject {
 public:
@@ -9,7 +10,7 @@ public:
 	~TransformObject();
 
 	bool IsOverlapping(TransformObject* OtherObject);
-
+	std::vector<TransformObject*> GetAllOverlaps();
 public:
 	Vector2D position;
 	float scale = 20.0f;
