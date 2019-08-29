e00cb18cc95e341e3af1c540a687460b56fa4599
JetDesktop <jethro0610@outlook.com>
2019-08-29 02:35:06 -0700

    Update main.cpp

--- EndlessSword/main.cpp
+++ EndlessSword/main.cpp
@@ -6,6 +6,14 @@
 #include "Spawner.h"
 
 int main(int argc, char** argv) {
+	int spawnInterval;
+	std::cout << "What should the spawn interval be? (Minimum: 50) ";
+	std::cin >> spawnInterval;
+
+	if (spawnInterval < 50)
+		spawnInterval = 50;
+	std::cout << "Set spawn interval to " << spawnInterval << std::endl;
+
 	// Start SDL
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
 		std::cerr << "SDL failed" << std::endl;
@@ -20,7 +28,7 @@ int main(int argc, char** argv) {
 	TTF_Init();
 
 	GameWorld gameWorld = GameWorld(800, 800);
-	Spawner* spawner = new Spawner(100);
+	Spawner* spawner = new Spawner(spawnInterval);
 
 	GeoHole* hole = new GeoHole();
 	hole->position = Vector2D(400, 400);
